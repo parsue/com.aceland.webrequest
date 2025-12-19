7dc5753118c41283c63af35c1740d046ee66a7ee
ACELANDWORKSHOP\parsu <parsue@gmail.com>
2025-12-19 11:31:38 +0800

    add exceptions

---
+++ Runtime/Exceptions/HttpErrorException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace AceLand.WebRequest.Exceptions
+{
+    public sealed class HttpErrorException : Exception
+    {
+        public HttpErrorException(HttpStatusCode statusCode, string body)
+            : base($"HTTP {(int)statusCode} {statusCode}")
+        {
+            StatusCode = statusCode;
+            ResponseBody = body;
+        }
+        
+        public HttpStatusCode StatusCode { get; }
+        public string ResponseBody { get; }
+    }
+}
---
+++ Runtime/Exceptions/HttpServerErrorException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Net;
+
+namespace AceLand.WebRequest.Exceptions
+{
+    public sealed class HttpServerErrorException : Exception
+    {
+        public HttpServerErrorException(HttpStatusCode statusCode, string body)
+            : base($"HTTP {(int)statusCode} {statusCode}")
+        {
+            StatusCode = statusCode;
+            ResponseBody = body;
+        }
+        
+        public HttpStatusCode StatusCode { get; }
+        public string ResponseBody { get; }
+    }
+}
