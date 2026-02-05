d4f8142c552bfef4164e891eeff67d36c8f4a6ef
Parsue Choi <parsue@gmail.com>
2026-02-05 10:34:44 +0800

    Add ReadOnlyField attribute to sectionName and rootCaFingerprint

--- Runtime/Profiles/ApiSectionsProfile.cs
+++ Runtime/Profiles/ApiSectionsProfile.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using System.IO;
+using System.Security.Cryptography.X509Certificates;
 using AceLand.Library.Attribute;
 using AceLand.Library.Extensions;
 using AceLand.WebRequest.ProjectSetting;
@@ -9,7 +11,7 @@ namespace AceLand.WebRequest.Profiles
     [CreateAssetMenu(fileName = "Api Sections Profile", menuName = "AceLand/Api Sections Profile")]
     public class ApiSectionsProfile : ScriptableObject
     {
-        [SerializeField] private string sectionName;
+        [SerializeField, ReadOnlyField] private string sectionName;
         
         [Header("Auto Fill Headers")]
         [SerializeField] private List<HeaderData> headers = new();
@@ -20,12 +22,23 @@ namespace AceLand.WebRequest.Profiles
         [SerializeField] private string apiVersion;
         [SerializeField, ReadOnlyField] private string apiUrl;
         
+        [Header("Private Root CA")]
+        [SerializeField, ReadOnlyField] private string rootCaFingerprint;
+        
         public string SectionName => sectionName;
         public string ApiUrl => apiUrl;
         public IEnumerable<HeaderData> Headers => headers;
+        public string RootCaFingerprint => rootCaFingerprint;
+
+        private void OnEnable()
+        {
+            sectionName = this.name;
+        }
 
         private void OnValidate()
         {
+            sectionName = this.name;
+            
             var http = useHttps ? "https://" : "http://";
             var url = VerifyDomainText(domain);
             apiUrl = (domain.IsNullOrEmptyOrWhiteSpace() ? "" : $"{http}{url}") +
@@ -44,6 +57,46 @@ namespace AceLand.WebRequest.Profiles
             text = text.Trim('/');
 
             return text;
+        }
+
+        [InspectorButton(Expanded = true, Mode = InspectorButtonMode.DisabledInPlayMode)]
+        private void LoadRootCA(string fullPathToPemFile)
+        {
+            if (fullPathToPemFile.IsNullOrEmptyOrWhiteSpace())
+                return;
+            
+            if (!File.Exists(fullPathToPemFile))
+            {
+                Debug.LogWarning($"{fullPathToPemFile} not found");
+                return;
+            }
+
+            if (!Path.HasExtension(fullPathToPemFile) || Path.GetExtension(fullPathToPemFile) != ".pem")
+            {
+                Debug.LogWarning("File is not a PEM file");
+                return;
+            }
+            
+            var fileBytes = File.ReadAllBytes(fullPathToPemFile);
+            var cert = new X509Certificate2(fileBytes);
+
+            rootCaFingerprint = cert.GetCertHashString();
+            
+            Debug.Log($"Loaded root ca certificate: {rootCaFingerprint}");
+            
+#if UNITY_EDITOR
+            UnityEditor.EditorUtility.SetDirty(this);
+#endif
+        }
+
+        [InspectorButton(Mode = InspectorButtonMode.DisabledInPlayMode)]
+        private void RemoveRootCA()
+        {
+            rootCaFingerprint = string.Empty;
+            
+#if UNITY_EDITOR
+            UnityEditor.EditorUtility.SetDirty(this);
+#endif
         }
     }
 }
